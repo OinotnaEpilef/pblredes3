4548d6d618570adacf767b31b58b9315cad6ab41
OinotnaEpilef <felipetompsomf18@gmail.com>
2024-12-16 15:03:52 -0300

    .

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database', 'db.sqlite3')}"
-GANACHE_URL = "http://127.0.0.1:8545"
+GANACHE_URL = "http://127.0.0.1:7545"
 CONTRACT_COMPILED_PATH = os.path.join(BASE_DIR, "contracts", "__compiled.json")
