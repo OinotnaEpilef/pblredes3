0a8d655050b1927e8fd0f5396c0c60346901a422
OinotnaEpilef <felipetompsomf18@gmail.com>
2024-12-16 15:35:09 -0300

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import EventDetails from './pages/EventDetails';
+import Home from './pages/Home.tsx';
+import EventDetails from './pages/EventDetails.tsx';
 
 const App: React.FC = () => {
   return (
