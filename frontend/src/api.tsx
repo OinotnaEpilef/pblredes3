64794e9c7df4e87397532706687de31552ba33c2
OinotnaEpilef <felipetompsomf18@gmail.com>
2024-12-16 16:50:21 -0300

    .

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5000';
+const API_BASE_URL = 'http://127.0.0.1:5454';
 
 export const getEvents = async () => {
   const response = await axios.get(`${API_BASE_URL}/events`);
